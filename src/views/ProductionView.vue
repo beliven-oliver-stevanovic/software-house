41968e1d90e7a31d1f9d0f602ee92c6131494898
Oliver Stevanovic <158463891+beliven-oliver-stevanovic@users.noreply.github.com>
2024-05-09 09:53:22 +0200

    feat: add main views

---
+++ src/views/ProductionView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useGameStore } from '@/stores/gameStore'
+
+const gameStore = useGameStore()
+</script>
+
+<template>
+  <h1>Production</h1>
+  <p>Budget: {{ gameStore.budget }}</p>
+  <ul>
+    <li v-for="dev in gameStore.devs" :key="dev.id">
+      {{ dev.getName() }} - {{ dev.getSeniority() }} -
+      {{ dev.getIsOccupied() ? 'Working...' : 'Free' }}
+    </li>
+  </ul>
+  <ul>
+    <li v-for="project in gameStore.projects" :key="project.id">
+      {{ project.getName() }} - {{ project.getComplexity() }} - {{ project.getValue() }}
+    </li>
+  </ul>
+</template>
