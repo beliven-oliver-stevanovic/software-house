d14d1cff11258c33cc205dbac68e02394896eeeb
Oliver Stevanovic <158463891+beliven-oliver-stevanovic@users.noreply.github.com>
2024-05-10 12:55:54 +0200

    refactor: remove code repetition

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # package
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
+The following are the instructions to get started and run the project on localhost.
 
 ## Project Setup
 
@@ -20,16 +12,5 @@ npm install
 
 ```sh
 npm run dev
-```
 
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
 ```
--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -1,43 +1,69 @@
 import { defineStore } from 'pinia'
 import Dev from '@/models/Dev'
+import Commercial from '@/models/Commercial'
+import { generateEmployeeName } from '@/utils'
 
 export const useGameStore = defineStore('game', {
   state: () => {
     return {
-      devs: [new Dev('First', 'Junior', 300)],
-      sales: [],
+      devs: [new Dev(generateEmployeeName(), 'Junior')],
+      commercials: [new Commercial(generateEmployeeName(), 'Junior')],
       projects: [],
       budget: 5000
     }
   },
 
   actions: {
-    hireDev(name) {
-      this.devs.push({ name })
+    hireCandidate(candidate) {
+      if (candidate instanceof Dev) {
+        this.devs = [...this.devs, candidate]
+      } else if (candidate instanceof Commercial) {
+        this.commercials = [...this.commercials, candidate]
+      }
     },
-    hireSales(name) {
-      this.sales.push({ name })
+    addProject(project) {
+      this.projects.push(project)
     },
-    addProject(name, value, complexity) {
-      this.projects.push({ name, value, complexity })
+    completeProject(projectId) {
+      let completed = this.projects.find((project) => project.id !== projectId)
+      this.budget += completed.value
+      this.projects = this.projects.filter((project) => project.id !== projectId)
     },
     decreaseBudget(amount) {
       this.budget -= amount
     },
     increaseBudget(amount) {
       this.budget += amount
+    },
+    assignProjectById(devId, projectId) {
+      let dev = this.devs.find((dev) => dev.id === devId)
+      let project = this.projects.find((project) => project.id === projectId)
+      project.isAssigned = true
+      dev.assignProject(project)
     }
   },
 
   getters: {
+    workDay() {
+      this.devs.forEach((dev) => dev.work())
+      this.commercials.forEach((commercial) => {
+        if (commercial.foundProject) {
+          this.projects.push(commercial.foundProject)
+          commercial.foundProject = null
+          commercial.searchTime = commercial.setSearchTime()
+        } else {
+          commercial.search()
+        }
+      })
+    },
     totalDevSalaries(state) {
       return state.devs.reduce((acc, dev) => acc + dev.getCost(), 0)
     },
-    totalSalesSalaries(state) {
-      return state.sales.reduce((acc, sale) => acc + sale.getCost(), 0)
+    totalCommercialsSalaries(state) {
+      return state.commercials.reduce((acc, commercial) => acc + commercial.getCost(), 0)
     },
     totalSalaries() {
-      return this.totalDevSalaries() + this.totalSalesSalaries()
+      return this.totalDevSalaries + this.totalCommercialsSalaries
     }
   }
 })
