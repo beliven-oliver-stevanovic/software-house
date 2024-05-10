a13906c8313a892eb1bd9a150468255bcfd466bd
Oliver Stevanovic <158463891+beliven-oliver-stevanovic@users.noreply.github.com>
2024-05-10 12:52:58 +0200

    feat: add components to refactor layout

---
+++ src/components/ListCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <ul>
+      <hr />
+      <slot name="elements">
+        <p>Nessun elemento disponibile</p>
+      </slot>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  gap: 1rem;
+  width: 90%;
+  background-color: white;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  border-radius: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/ListElement.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  element: Object,
+  button: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <li>
+    <div class="container">
+      <span v-for="(value, key) in element" :key="key">
+        <p v-if="key != 'id' && key != 'labels'">
+          <strong>{{ element.labels[key] }}</strong
+          >: {{ value }}
+        </p>
+      </span>
+      <button v-if="button" @click="button.onClickFunction()">{{ button.label }}</button>
+    </div>
+    <hr />
+  </li>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0.5rem;
+  border-radius: 20px;
+}
+</style>
